d851810e24b4fa3eafd07c8f74771b7054b2c256
marialejandravc <159529153+marialejandravc@users.noreply.github.com>
2024-06-12 11:58:35 +0000

    add icons from fontawesome

---
+++ src/js/icons.js
@@ -0,0 +1,8 @@
+// /* global FontAwesomeConfig */
+ import fontawesome from '@fortawesome/fontawesome';
+ import faClock from '@fortawesome/fontawesome-free-regular/faClock';
+ fontawesome.config = {
+   autoReplaceSvg: 'nest'
+ };
+ fontawesome.library.add(faClock);
+
